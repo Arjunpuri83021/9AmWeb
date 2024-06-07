eb95cd15ec87194d08dbb546281e48c905c80357
arjun puri <thevirus991@gmail.com>
2024-06-07 10:36:36 +0530

    first

---
+++ js/index.js
@@ -0,0 +1,135 @@
+///var , let , const
+
+
+
+// const a = 24
+
+// console.log(a)
+
+
+
+// let a = 2
+ 
+// a-=5
+
+// console.log(a)
+
+
+// let a = 4
+// let b = "arjun"
+
+// let c = a >=b || a<=b ? "this is true" : "this is false"
+
+// console.log(c)
+
+
+// let a = 4+ "5"
+
+// console.log(a)
+
+
+// function handel(){
+//    let a = 4
+// let b = "arjun"
+
+// let c = a >=b || a<=b ? "this is true" : "this is false"
+
+// console.log(c)
+    
+// }
+
+// handel()
+
+
+// const handel = (a,b)=>{
+//  console.log(a+b)
+// }
+
+// handel(2,5)
+
+// handel(5,7)
+
+
+// function add(a,b,c,d){
+// console.log(arguments)
+// return a+b+c+d
+// }
+// add(10,15,3,4)
+
+
+// const add = (a,b)=>{
+//     console.log(arguments)
+//     return a+b
+
+// }
+
+// add(10,15)
+
+
+
+// function handel(){
+
+// }
+
+// const handel2 = function(){
+
+// }
+
+
+
+// let obj = {name:"arjun",age:24,city:"jaipur"}
+
+// console.log("my name is",obj.name,"and my age is",obj.age,"and my city is",obj.city)
+
+
+// let age = 60;
+
+// switch(true){
+     
+//     case age >=12 && age <=17:
+//         console.log("you are child")
+//         break;
+
+//     case age >=18 && age <=25:
+//     console.log("you are adult");
+//      break;
+
+//      case age >=26 && age <=35:
+//         console.log("your Older")
+//         break;
+     
+//         case age >=36:
+//             console.log("Your deathed")
+//           break;  
+
+//       default:
+//         console.log("not matched")  
+// }
+
+
+
+// let age =17;
+
+// if(age >=18 && age <=25){
+//     console.log("adult")
+// }
+
+// else if (age >=26 && age <=35){
+//     console.log("younger")
+// }
+
+// else{
+//     console.log("not matched")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
